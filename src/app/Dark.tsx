ee805e9ea2c9f461d6846eded256e04700021d73
ojinikim <ojinikim@gmail.com>
2024-05-16 23:35:22 +0900

    ADD :: change dark/light mode

---
+++ src/app/Dark.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Dark() {
+  return (
+    <div className="absolute bottom-4 right-4 w-10 h-10 rounded-full bg-red600 dark:bg-green600"></div>
+  );
+}
+
+export default Dark;
