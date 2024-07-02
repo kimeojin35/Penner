43d75cd8995ca72afa94305d9502b9262bd8333c
ojinikim <ojinikim@gmail.com>
2024-07-02 23:50:27 +0900

    add :: svg 추가

---
+++ src/assets/Arrow.tsx
@@ -0,0 +1,42 @@
+interface PropsType {
+  size?: number;
+  onClick?: () => void;
+  className?: string;
+  direction?: "left" | "top" | "right" | "bottom";
+}
+
+export const Arrow = ({
+  size = 24,
+  className = "",
+  onClick,
+  direction = "left",
+}: PropsType) => {
+  const rotate = {
+    right: "rotate-[0deg]",
+    top: "rotate-[90deg]",
+    left: "rotate-[180deg]",
+    bottom: "rotate-[270deg]",
+  };
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 24 24"
+      onClick={onClick}
+      className={`${onClick ? "cursor-pointer" : ""} ${className} ${
+        rotate[direction]
+      }`}
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-opacity=".2"
+        stroke-width="2"
+        d="m9 19 7-7-7-7"
+      />
+    </svg>
+  );
+};
---
+++ src/assets/Heart.tsx
@@ -0,0 +1,34 @@
+interface PropsType {
+  size?: number;
+  onClick?: () => void;
+  className?: string;
+  clicked?: boolean;
+}
+
+export const Heart = ({
+  size = 24,
+  onClick,
+  className = "",
+  clicked,
+}: PropsType) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 24 24"
+      onClick={onClick}
+      className={`${onClick ? "cursor-pointer" : ""} ${className}`}
+    >
+      <path
+        fill={clicked ? "currentColor" : "none"}
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 7.693C10 3 3 3.5 3 9.5s9 11 9 11 9-5 9-11-7-6.5-9-1.806Z"
+      />
+    </svg>
+  );
+};
