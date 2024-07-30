c8242f87dfa0059572396bbfab866cf92200ec55
ojinikim <ojinikim@gmail.com>
2024-07-30 23:46:32 +0900

    add :: 404 페이지

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,14 @@ import { useRouter } from "next/navigation";
 export default function NotFound() {
   const router = useRouter();
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-white dark:bg-gray900">
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-white dark:bg-gray900">
       <p className="text-black dark:text-white text-title24Bold">404</p>
-      <button onClick={() => router.push("/")}>뒤로가기</button>
+      <button
+        className="dark:text-white text-gray900"
+        onClick={() => router.back()}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 }
