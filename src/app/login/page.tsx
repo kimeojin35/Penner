95a0384314c3e9b8e139c9749752b8e338d9f14e
ojinikim <ojinikim@gmail.com>
2024-08-12 20:30:12 +0900

    add :: 로그인 페이지 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,13 @@
+"use client";
 import { createClient } from "@/utils/supabase/server";
+import React from "react";
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gray50 dark:bg-gray950">
+      <p className="text-medium16 dark:text-white">로그인 페이지</p>
+    </div>
+  );
+}
+
+export default Login;
