6d961d0c73f9f5a5014c36d9eeaaa2c127db95b1
ojinikim <ojinikim@gmail.com>
2024-09-17 23:59:09 +0900

    edit :: Input props 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,30 +1,43 @@
-import { Show } from "@/assets";
-import React from "react";
+"use client";
+import { Hide, Show } from "@/assets";
+import React, { useState } from "react";
 
 interface PropsType {
   placeholder?: string;
-  type: string;
   password?: boolean;
   title?: string;
+  children?: React.ReactNode;
+  onChange?: (value: any) => void;
 }
 
-export const Input = ({ placeholder, type, password, title }: PropsType) => {
+export const Input = ({
+  placeholder,
+  password,
+  title,
+  children,
+  onChange,
+}: PropsType) => {
+  const [click, setClick] = useState<boolean>(false);
   return (
     <div className="w-full flex flex-col gap-2">
-      <p className="text-bold14">{title}</p>
+      <p className="text-medium16">{title}</p>
       <div className="flex w-full items-center rounded-lg border border-gray300">
         <input
-          type={type}
+          onChange={(e) => onChange && onChange(e.target.value)}
+          type={password && !click ? "password" : "text"}
           placeholder={placeholder}
-          className="p-4 placeholder:text-gray300"
+          className="p-4 placeholder:text-gray300 text-medium18 w-full"
         />
-        {password ? (
-          <div className="flex p-4">
-            <Show className="text-gray700" />
+        {password && (
+          <div onClick={() => setClick(!click)} className="flex p-4">
+            {click ? (
+              <Hide className="text-gray700" />
+            ) : (
+              <Show className="text-gray700" />
+            )}
           </div>
-        ) : (
-          <></>
         )}
+        {children}
       </div>
     </div>
   );
