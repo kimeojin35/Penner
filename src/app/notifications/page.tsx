f0438d94ec7d217631d4afb3897d3ad3471b633c
ojinikim <ojinikim@gmail.com>
2024-09-22 23:44:25 +0900

    add :: 알림 기본 페이지

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notifications() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gray50 dark:bg-gray950">
+      <p className="text-medium16 dark:text-white">알림 페이지</p>
+    </div>
+  );
+}
+
+export default Notifications;
