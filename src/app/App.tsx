6f8730c45390630a24395a0d0c1cb3aa3c7666c7
ojinikim <ojinikim@gmail.com>
2024-09-22 23:41:56 +0900

    del :: 테마변경버튼 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,7 @@ import { useState, useEffect } from "react";
 import useDarkMode from "../hooks/useDarkMode";
 
 function App() {
-  const [dark, toggleDarkMode] = useDarkMode();
-  return (
-    <div
-      onClick={toggleDarkMode}
-      className="cursor-pointer absolute flex px-4 py-2 rounded-full bottom-10 right-10 bg-white dark:bg-gray800 text-black dark:text-white text-title20Bold"
-    >
-      <button>{dark ? "Dark" : "Light"}</button>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
