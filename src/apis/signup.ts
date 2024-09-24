6b796dc888b2f0e56a62ea9a9d0c1f1210074c0d
ojinikim <ojinikim@gmail.com>
2024-09-24 23:44:46 +0900

    feat :: 회원가입 api 연동

---
+++ src/apis/signup.ts
@@ -0,0 +1,57 @@
+import { createClient } from "@/utils/supabase/client";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const supabase = createClient();
+
+export const signupHandler = async (
+  data: any,
+  router: string[] | AppRouterInstance
+) => {
+  try {
+    const { email, password, nickname } = data;
+
+    // 먼저 이메일이 이미 존재하는지 확인
+    const { data: existingUser, error: checkError } = await supabase
+      .from("user")
+      .select("email")
+      .eq("email", email);
+
+    if (existingUser && existingUser.length > 0) {
+      console.error("Email already exists.");
+      return;
+    }
+
+    const { data: signupData, error: signupError } = await supabase.auth.signUp(
+      {
+        email,
+        password,
+      }
+    );
+
+    // if (signupError) {
+    //   console.error("Signup error:", signupError);
+    //   return;
+    // }
+
+    // 새로운 유저 정보 추가
+    const user = signupData?.user;
+
+    // if (!user) {
+    //   console.error("No user returned after signup");
+    //   return;
+    // }
+
+    const { error: userInfoError } = await supabase.from("user").insert({
+      id: user?.id, // Supabase 유저 ID
+      email: email,
+      nickname: nickname,
+      password: password,
+    });
+
+    // if (userInfoError) {
+    //   console.error("Error inserting into userinfo:", userInfoError);
+    //   return;
+    // }
+    router.push("/login");
+  } catch (error) {}
+};
--- src/apis/user.ts
+++ src/apis/user.ts
@@ -2,26 +2,32 @@ import { createClient } from "@/utils/supabase/client";
 import { FormEvent } from "react";
 const supabase = createClient();
 
-const signupHandler = async (e: FormEvent) => {
-  e.preventDefault();
-  try {
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-    });
-    if (error) console.error(error);
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const signupHandler = async (e: FormEvent) => {
+//   e.preventDefault();
+//   try {
+//     const { data, error } = await supabase.auth.signUp({
+//       email: email,
+//       password: password,
+//       options: {
+//         data: {
+//           user_name: name,
+//           message: "",
+//         },
+//       },
+//     });
+//     if (error) console.error(error);
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
 const loginHandler = async (e: FormEvent) => {
   e.preventDefault();
   try {
     const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
+      email: email,
+      password: password,
     });
     if (error) console.error(error);
     if (data) {
@@ -33,4 +39,8 @@ const loginHandler = async (e: FormEvent) => {
   } catch (error) {
     console.error(error);
   }
+};
+
+const logoutHandler = async () => {
+  const { error } = await supabase.auth.signOut();
 };
