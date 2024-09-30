7703a74c443ea6a620f22b7863018cc3ce75c087
ojinikim <ojinikim@gmail.com>
2024-09-30 23:58:47 +0900

    add :: toast 퍼블리싱

--- src/utils/toast/BoardCreate.tsx
+++ src/utils/toast/BoardCreate.tsx
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 
 function BoardCreate() {
   const [toast, setToast] = useState<boolean>(false);
+  const onSubmit = (data) => {
+    if (!data.title.trim() || !data.content.trim()) {
+      setError("title", { type: "trim" });
+      setToast(true);
+      return;
+    }
+  };
   return <></>;
 }
 
---
+++ src/utils/toast/ToastPopup.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+
+interface ToastProps {
+  message: string;
+  setToast: any;
+  position: string;
+  type: "default" | "error" | "fail" | "success";
+}
+
+export default function ToastPopup({
+  message,
+  setToast,
+  position,
+  type,
+}: ToastProps) {
+  const toastType = {
+    default: "",
+    error: "",
+    fail: "",
+    success: "",
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setToast]);
+  return (
+    <div
+      className={`${
+        toastType[type]
+      } fixed z-20 flex h-[4rem] w-[90%] max-w-[73rem] items-center justify-center rounded-[1rem] bg-green-50 opacity-[97%] shadow-[0px_2px_8px_rgba(0,0,0,0.25)] ${
+        position === "top" ? "animate-toast-top" : "animate-toast-bottom"
+      }`}
+    >
+      <p className="text-Body text-white">{message}</p>
+    </div>
+  );
+}
